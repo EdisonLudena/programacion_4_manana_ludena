e5473326eb97b93dbcaabd7be04b6ee0c49408bb
Edison <premusic314@gmail.com>
2026-04-08 11:04:50 -0500

    first commit

---
+++ modulo_1/basics/05_readline.kt
@@ -0,0 +1,27 @@
+fun main() {
+    
+    //Readline
+    println("Escribe tu nombre: ")
+    val nombre = readLine()
+    println("Nombre incluido: $nombre")
+    
+    println("Escribe tu apellido: ")
+    val apellido = readLine() ?: "anónimo"
+    println("Apellido incluido: $apellido")
+    
+    /*
+    Comentarios multilinea
+    */
+    
+    //Comentariio de una sola linea
+    
+    /**
+    *Multilineas
+    *Comentarios
+    *@param nombre del usuario
+    *@return un saludo personalizado
+    */
+} 
+
+
+
