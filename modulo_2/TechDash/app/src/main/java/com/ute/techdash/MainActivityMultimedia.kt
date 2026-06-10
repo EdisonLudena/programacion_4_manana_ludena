a914adfa48c289087aa8d4c18d8c197e16c89af1
alumnos <edison.ludena.wlq@gmail.com>
2026-06-10 05:42:58 -0500

    CHECK 10-06-2026

--- modulo_2/TechDash/app/src/main/java/com/ute/techdash/MainActivityMultimedia.kt
+++ modulo_2/TechDash/app/src/main/java/com/ute/techdash/MainActivityMultimedia.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.ute.techdash.iu.hardware.sensores.PantallaSensores
+import com.ute.techdash.iu.multimedia.PantallaMultimedia
 import com.ute.techdash.ui.theme.TechDashTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivityMultimedia : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TechDashTheme {
-                PantallaSensores()
+                PantallaMultimedia()
             }
         }
     }
